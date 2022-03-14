9492c7cac30bdaca0406db0c4c2e7fac3d66036e
vinodkumar1215 <vk.vinodkumar1215@gmail.com>
2022-03-14 14:14:16 +0530

    product,pagination interface added. End of section 8

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,6 +36,7 @@ namespace API
                 opt.AddPolicy("CorsPolicy", policy =>
                 {
                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
                 });
             });  
             

90003abf7f61becf12dd39366f72a44bc33043a0
vinodkumar1215 <vk.vinodkumar1215@gmail.com>
2022-03-25 18:17:26 +0530

    end of section 12, loading functionality  added

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
+ 
  
 
   constructor(){}

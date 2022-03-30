49a5658d7b7f8bd7325f229ad44f8bba115d93ea
vinodkumar1215 <vk.vinodkumar1215@gmail.com>
2022-03-30 15:25:59 +0530

    End of section13

--- API/Startup.cs
+++ API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
 
 namespace API
 {
@@ -28,6 +29,12 @@ namespace API
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddDbContext<StoreContext>(x =>
              x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+
+             services.AddSingleton<IConnectionMultiplexer>(c => {
+                 var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"),
+                 true);
+                 return ConnectionMultiplexer.Connect(configuration);
+             });
 
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();

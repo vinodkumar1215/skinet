129e1fd3bc8c82dec82faa222aca00708dfdab43
vinodkumar1215 <vk.vinodkumar1215@gmail.com>
2022-04-01 09:55:35 +0530

    end of section14

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { BasketService } from './basket/basket.service';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,16 @@ export class AppComponent implements OnInit {
  
  
 
-  constructor(){}
+  constructor(private basketService: BasketService){}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe(() => {
+        console.log('initialised basket');
+      }, error => {
+        console.log(error);
+      });
+    }
+  }
 }

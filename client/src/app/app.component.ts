01523766f3d90db3197ab22773a47e8318bcb0eb
vinodkumar1215 <vk.vinodkumar1215@gmail.com>
2022-05-10 18:16:39 +0530

    End of section 17, Client Api Completed

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 
 @Component({
@@ -11,9 +12,23 @@ export class AppComponent implements OnInit {
  
  
 
-  constructor(private basketService: BasketService){}
+  constructor(private basketService: BasketService, private accountService: AccountService){}
 
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+      this.accountService.loadCurrentUser(token).subscribe(() => {
+        console.log('loaded user');
+      }, error => {
+        console.log(error);
+      });
+  }
+
+  loadBasket() {
     const basketId = localStorage.getItem('basket_id');
     if (basketId) {
       this.basketService.getBasket(basketId).subscribe(() => {
